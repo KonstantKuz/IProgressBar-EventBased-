3aa9147e13e8b6e7a04e592e90e2a9e1822e3b93
Konstantin Kuznetsov <stalkercatkuz@gmail.com>
2020-04-20 22:08:51 +0300

    Added actions without params

--- Assets/Scripts/EventAggregator/EventAggregator.cs
+++ Assets/Scripts/EventAggregator/EventAggregator.cs
@@ -5,9 +5,58 @@ using UnityEngine;
 
 namespace EventAggregation
 {
+    public interface IEventBase { }
+
+    public interface IEventSubscriber
+    {
+        void SubscribeToNecessaryEvents();
+    }
+    
+    public interface IEventPublisherWithParams
+    {
+        void PublishWithParams<T>(T publishedEvent) where T : class;
+    }
+
+    public interface IEventPublisherWithOutParams
+    {
+        void Publish<T>() where T : IEventBase;
+    }
+
     public static class EventAggregator
     {
         private static Dictionary<Type, List<Action<IEventBase>>> _listeners = new Dictionary<Type, List<Action<IEventBase>>>();
+        private static Dictionary<Type, List<Action>> _listeners_Simple = new Dictionary<Type, List<Action>>();
+
+        public static void Subscribe<T>(Action listener) where T : class
+        {
+            if(!_listeners_Simple.ContainsKey(typeof(T)))
+            {
+                _listeners_Simple.Add(typeof(T), new List<Action>());
+            }
+
+            _listeners_Simple[typeof(T)].Add(listener);
+        }
+
+        public static void Publish<T>()
+        {
+            if (!_listeners_Simple.ContainsKey(typeof(T)))
+            {
+                return;
+            }
+
+            foreach (var action in _listeners_Simple[typeof(T)])
+            {
+                action.Invoke();
+            }
+        }
+
+        public static void Unsubscribe<T>(Action action) where T : class
+        {
+            if (_listeners_Simple.ContainsKey(typeof(T)))
+            {
+                _listeners_Simple[typeof(T)].Remove(action);
+            }
+        }
 
         public static void Subscribe<T>(Action<IEventBase> listener) where T : class
         {
