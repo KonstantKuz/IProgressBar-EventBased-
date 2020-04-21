ceaf9ccc9f11ba3ea4bb65c1887a85b36ed95346
Konstantin Kuznetsov <stalkercatkuz@gmail.com>
2020-04-21 20:11:08 +0300

    Cleaned READme & IProgressBar

--- Assets/Scripts/IProgressBar.cs
+++ Assets/Scripts/IProgressBar.cs
@@ -2,180 +2,6 @@
 using UnityEngine.UI;
 using System;
 
-/// все это должно помочь легко управлять любыми видами прогресса на сцене и разными видами прогресс баров
-/// то есть поможет изи следить и за прогрессом фактическим (триггеря/вызывая необходимые события/методы при достижении прогресса)
-/// и за прогрессом визуальным 
-/// + с минимальным количеством кода в классах использующих прогрессы
-/// + с максимально шаблонным и однотипным кодом в классах использующих прогрессы
-/// без необходимости делать из него сервис
-/// удобство в том, что независимо от типа и вида прогресса и прогресс бара обращение к нему производится одними и теми же способами
-/// в случае одиночных прогресс баров - обращение с ними происходит строго через три дженерик события
-/// в случае множества прогресс баров - обращение через методы интерфейса посредством их вызова из экземпляра класса в котором нужно следить и визуализировать прогресс
-/// (например здоровье машинки которых в сцене может быть много - управление прогресс баром производится из объекта машинки без какой либо необходимости менять код прогресс бара)
-/// 
-/// то есть в итоге независимо от типа и вида прогресс бара обращение с ним будет максимально однотипным
-/// все прогрессы будут работать по шаблону == быстрее будем работать)
-/// 
-/// по идее все должно быть так))
-/// 
-/// 
-/// КАК ПОЛЬЗОВАТЬСЯ SceneLineProgressBar/ScenePointsProgressBar:
-/// допустим нужен новый прогресс бар в сцене например как прогрессбар здоровья дерева
-/// создаем новый скрипт и просто наследуем его от нужного типа прогресс бара
-/// в нашем случае LineProgressBar
-/// 
-/// в итоге мы должны получить ПУСТОЙ скрипт
-public class HealthExampleTreeProgressBar : SceneLineProgressBar<HealthExampleTreeProgressBar>
-{
-}
-/// этот скрипт вешаем на прогресс бар и вставляем в поле нужную картинку которая исполняет роль прогрессбара (то есть заполняется/убавляется с помощью свойства fillAmount)
-/// сам прогресс бар готов, инвертировать визуальное направление прогресса можно с помощью RevertVisual
-/// далее в нашем случае например в скрипте дерева нам нужно инициализировать его с помощью события следущим образом
-/// InitializeProgressIn<TreeHealthProgressBar>
-/// 
-public class ExampleTree
-{
-    private float maxHealth;
-    private float currentHealth;
-
-    InitialData<HealthExampleTreeProgressBar> healthInitData;
-    UpdateData<HealthExampleTreeProgressBar> healthUpdateData;
-
-    void OnEnable()
-    {
-        SubscribeToNecessaryEvents();
-    }
-
-    public void SubscribeToNecessaryEvents()
-    {
-        HealthExampleTreeProgressBar.OnProgressFinished += Cut;
-    }
-
-    void Cut(OnFinishProgress<HealthExampleTreeProgressBar> stageCompleteData)
-    {
-        //DEATH
-    }
-
-    void Start()
-    {
-        InitializeHealthBar();
-    }
-
-    void InitializeHealthBar()
-    {
-        healthInitData.MinValue = 0;
-        healthInitData.MaxValue = maxHealth;
-        healthInitData.CurrentValue = maxHealth; 
-
-        HealthExampleTreeProgressBar.InitializeProgress.Invoke(healthInitData);
-    }
-
-    void ApplyDamage(float amount)
-    {
-        currentHealth -= amount;
-        UpdateHealthBar();
-    }
-
-    void UpdateHealthBar()
-    {
-        healthUpdateData.CurrentValue = currentHealth;
-        HealthExampleTreeProgressBar.UpdateProgress.Invoke(healthUpdateData);
-    }
-
-}
-/// И похожий пример контроллера в случае если например нужно срубить несколько деревьев
-public class StageExampleTreeProgressBar : SceneLineProgressBar<StageExampleTreeProgressBar>
-{
-}
-public class ExampleTreeController
-{
-    private int treesCountToWin;
-    private int cuttedTreesCount;
-
-    InitialData<StageExampleTreeProgressBar> stageInitData;
-    UpdateData<StageExampleTreeProgressBar> stageUpdateData;
-
-    void OnEnable()
-    {
-        SubscribeToNecessaryEvents();
-    }
-
-    public void SubscribeToNecessaryEvents()
-    {
-        StageExampleTreeProgressBar.OnProgressFinished += FinishGame;
-    }
-
-    void Start()
-    {
-        InitializeStageBar();
-    }
-
-    void InitializeStageBar()
-    {
-        stageInitData.MinValue = 0;
-        stageInitData.MaxValue = treesCountToWin;
-        stageInitData.CurrentValue = 0;
-
-        StageExampleTreeProgressBar.InitializeProgress.Invoke(stageInitData);
-    }
-
-    void InstantiateNewTree()
-    {
-        cuttedTreesCount++;
-        //Instantiate new tree
-        UpdateStageProgress();
-    }
-
-    void UpdateStageProgress()
-    {
-        stageUpdateData.CurrentValue = cuttedTreesCount;
-        StageExampleTreeProgressBar.UpdateProgress.Invoke(stageUpdateData);
-    }
-
-    void FinishGame(OnFinishProgress<StageExampleTreeProgressBar> stageCompleteData)
-    {
-        //Finish
-    }
-}
-
-/// с GameObjectLineProgressBar все проще
-/// создаем прогресс бар и вешаем на него такой же пустой скрипт
-/// цепляем этот прогресс бар на объект и в классе объекта создаем ссылку на этот бар
-public class ExampleGOLineProgressBar : GOLineProgressBar
-{
-}
-public class ExampleGOWithHealth
-{
-    [SerializeField] private ExampleGOLineProgressBar healthBar;
-    private float currentHP;
-    private float maxHP;
-
-    void OnEnable()
-    {
-        healthBar.OnProgressFinished += Death;
-    }
-
-    void Death()
-    {
-
-    }
-
-    void Start()
-    {
-        InitializeHealthBar();
-    }
-
-    void InitializeHealthBar()
-    {
-        healthBar.Initialize(0, maxHP, currentHP);
-    }
-
-    void ApplyDamage(float damage)
-    {
-        currentHP -= damage;
-        healthBar.UpdateCurrentProgress(currentHP);
-    }
-}
 public interface IProgressBar
 {
     bool Finished { get; }         // необходимо для одноразового вызова события OnProgressFinished<КонкретныйПрогрессБар>
@@ -617,5 +443,165 @@ public class GOLineProgressBar : MonoBehaviour, GameObjectProgressBar
                 }
             }
         }
+    }
+}
+
+
+
+/// КАК ПОЛЬЗОВАТЬСЯ SceneLineProgressBar/ScenePointsProgressBar:
+/// допустим нужен новый прогресс бар в сцене например как прогрессбар здоровья дерева
+/// создаем новый скрипт и просто наследуем его от нужного типа прогресс бара
+/// в нашем случае LineProgressBar
+/// 
+/// в итоге мы должны получить ПУСТОЙ скрипт
+public class HealthExampleTreeProgressBar : SceneLineProgressBar<HealthExampleTreeProgressBar>
+{
+}
+/// этот скрипт вешаем на прогресс бар и вставляем в поле нужную картинку которая исполняет роль прогрессбара (то есть заполняется/убавляется с помощью свойства fillAmount)
+/// сам прогресс бар готов, инвертировать визуальное направление прогресса можно с помощью RevertVisual
+/// далее в нашем случае например в скрипте дерева нам нужно инициализировать его с помощью события следущим образом
+/// InitializeProgressIn<TreeHealthProgressBar>
+/// 
+public class ExampleTree
+{
+    private float maxHealth;
+    private float currentHealth;
+
+    InitialData<HealthExampleTreeProgressBar> healthInitData;
+    UpdateData<HealthExampleTreeProgressBar> healthUpdateData;
+
+    void OnEnable()
+    {
+        SubscribeToNecessaryEvents();
+    }
+
+    public void SubscribeToNecessaryEvents()
+    {
+        HealthExampleTreeProgressBar.OnProgressFinished += Cut;
+    }
+
+    void Cut(OnFinishProgress<HealthExampleTreeProgressBar> stageCompleteData)
+    {
+        //DEATH
+    }
+
+    void Start()
+    {
+        InitializeHealthBar();
+    }
+
+    void InitializeHealthBar()
+    {
+        healthInitData.MinValue = 0;
+        healthInitData.MaxValue = maxHealth;
+        healthInitData.CurrentValue = maxHealth;
+
+        HealthExampleTreeProgressBar.InitializeProgress.Invoke(healthInitData);
+    }
+
+    void ApplyDamage(float amount)
+    {
+        currentHealth -= amount;
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        healthUpdateData.CurrentValue = currentHealth;
+        HealthExampleTreeProgressBar.UpdateProgress.Invoke(healthUpdateData);
+    }
+
+}
+/// И похожий пример контроллера в случае если например нужно срубить несколько деревьев
+public class StageExampleTreeProgressBar : SceneLineProgressBar<StageExampleTreeProgressBar>
+{
+}
+public class ExampleTreeController
+{
+    private int treesCountToWin;
+    private int cuttedTreesCount;
+
+    InitialData<StageExampleTreeProgressBar> stageInitData;
+    UpdateData<StageExampleTreeProgressBar> stageUpdateData;
+
+    void OnEnable()
+    {
+        SubscribeToNecessaryEvents();
+    }
+
+    public void SubscribeToNecessaryEvents()
+    {
+        StageExampleTreeProgressBar.OnProgressFinished += FinishGame;
+    }
+
+    void Start()
+    {
+        InitializeStageBar();
+    }
+
+    void InitializeStageBar()
+    {
+        stageInitData.MinValue = 0;
+        stageInitData.MaxValue = treesCountToWin;
+        stageInitData.CurrentValue = 0;
+
+        StageExampleTreeProgressBar.InitializeProgress.Invoke(stageInitData);
+    }
+
+    void InstantiateNewTree()
+    {
+        cuttedTreesCount++;
+        //Instantiate new tree
+        UpdateStageProgress();
+    }
+
+    void UpdateStageProgress()
+    {
+        stageUpdateData.CurrentValue = cuttedTreesCount;
+        StageExampleTreeProgressBar.UpdateProgress.Invoke(stageUpdateData);
+    }
+
+    void FinishGame(OnFinishProgress<StageExampleTreeProgressBar> stageCompleteData)
+    {
+        //Finish
+    }
+}
+
+/// с GameObjectLineProgressBar все проще
+/// создаем прогресс бар и вешаем на него такой же пустой скрипт
+/// цепляем этот прогресс бар на объект и в классе объекта создаем ссылку на этот бар
+public class ExampleGOLineProgressBar : GOLineProgressBar
+{
+}
+public class ExampleGOWithHealth
+{
+    [SerializeField] private ExampleGOLineProgressBar healthBar;
+    private float currentHP;
+    private float maxHP;
+
+    void OnEnable()
+    {
+        healthBar.OnProgressFinished += Death;
+    }
+
+    void Death()
+    {
+
+    }
+
+    void Start()
+    {
+        InitializeHealthBar();
+    }
+
+    void InitializeHealthBar()
+    {
+        healthBar.Initialize(0, maxHP, currentHP);
+    }
+
+    void ApplyDamage(float damage)
+    {
+        currentHP -= damage;
+        healthBar.UpdateCurrentProgress(currentHP);
     }
 }
