bd9711c7a1542f6163996b4affbcab0cb282414b
Konstantin Kuznetsov <stalkercatkuz@gmail.com>
2020-05-05 21:58:47 +0300

    Clean&Fixes + stagebar example load button

--- Assets/Scripts/ProgressBarExamples/NextExample.cs
+++ Assets/Scripts/ProgressBarExamples/NextExample.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class NextExample : MonoBehaviour
 {
+    [SerializeField] private int sceneIndex = 0;
+
     private void Start()
     {
-        GetComponent<Button>().onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(1));
+        GetComponent<Button>().onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex));
     }
 }
